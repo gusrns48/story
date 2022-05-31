fd39d12e372fcb2d66a54b2dc7c915f9598ad353
gusrns48 <gusrns48@onoffmix.com>
2022-05-31 11:52:22 +0900

    home_font

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import About from "./component/about"
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
-    </div>
   );
 };
 
