c8c4ef0361d702a9352f69d5613172fa2e31357d
gusrns48 <gusrns48@onoffmix.com>
2022-06-02 17:43:06 +0900

    setting~

--- src/component/home.js
+++ src/component/home.js
@@ -1,83 +1,23 @@
 import { useEffect } from 'react'
 import '../style/home.scss'
 
+const icons = ['Office', 'Word', 'Excel', 'PowerPoint', 'Outlook', 'OneDrive', 'OneNote', 'ToDo', 'Calendar', 'Skype'];
+
 const Home = () => {
   useEffect(()=>{
-    window.addEventListener('mousemove', handleMouseMove);
-    window.addEventListener('resize', handleWindowResize);
-  
-    const spansSlow = document.querySelectorAll('.spanSlow');
-    const spansFast = document.querySelectorAll('.spanFast');
-  
-    let width = window.innerWidth;
-  
-    function handleMouseMove(e) {
-      let normalizedPosition = e.pageX / (width/2) - 1;
-      let speedSlow = 100 * normalizedPosition;
-      let speedFast = 200 * normalizedPosition;
-      spansSlow.forEach((span) => {
-        span.style.transform = `translate(${speedSlow}px)`;
-      });
-      spansFast.forEach((span) => {
-        span.style.transform = `translate(${speedFast}px)`
-      })
-    }
-    function handleWindowResize() {
-      width = window.innerWidth;
-    }
   });
 
   return(
-    <div class="wrap">
-      <div class="line">
-        <div class="left">
-          <div class="content">
-            <span class="spanSlow">Welcome</span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="content">
-            <span class="spanSlow">Welcome</span>
-          </div>
-        </div>
+    <section className='home'>
+      <div className='leftRail'>
+        <li className='Nine_Dots'></li>
+        {icons.map(icons =>
+          <li className={icons}></li>  
+        )}
+        <li className='Three_Dots'></li>
       </div>
-      <div class="line">
-        <div class="left">
-          <div class="content">
-            <span class="spanSlow">TO</span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="content">
-            <span class="spanSlow">TO</span>
-          </div>
-        </div>
-      </div>
-      <div class="line">
-        <div class="left">
-          <div class="content">
-            <span class="spanFast">GUS48</span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="content">
-            <span class="spanFast">GUS48</span>
-          </div>
-        </div>
-      </div>
-      <div class="line">
-        <div class="left">
-          <div class="content">
-            <span class="spanSlow">BLOG</span>
-          </div>
-        </div>
-        <div class="right">
-          <div class="content">
-            <span class="spanSlow">BLOG</span>
-          </div>
-        </div>
-      </div>
-    </div>
+      <div className='backgroundImageOverlay'></div>
+    </section>
   )
 }
 
